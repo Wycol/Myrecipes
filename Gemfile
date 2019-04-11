ca2cb7d9b538603c1b5b680d22bec9a899b7aae5
Antonio Perez <apcano1978@gmail.com>
2019-04-11 16:02:11 +0200

    Prelogin

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'jquery-rails'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -25,7 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+gem 'will_paginate', '~> 3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,56 @@
+class ChefsController < ApplicationController
+
+  def index
+    @chefs= Chef.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @chef= Chef.new
+  end
+
+  def create
+    @chef= Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Welcome #{@chef.chefname} to MyRecipees App!"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @chef = Chef.find(params[:id])
+    @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
+
+  end
+
+  def edit
+    @chef = Chef.find(params[:id])
+
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Chef was updated successfully!"
+            redirect_to chef_path(@chef)
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @chef = Chef.find(params[:id])
+    @chef.destroy
+    flash[:danger]="Chefs and all associated recipes have been deleted!"
+    redirect_to chefs_path
+
+  end
+
+private
+  def chef_params
+    params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+  end
+
+end
